cc27622bacfcc50e85317a1f9fad9146c424b203
vimalkottikkal <vimalvijay1994@gmail.com>
2024-11-21 16:01:52 +0400

    Weather App React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="search-action">
+        <form action='#' className="search-form">
+          <input type="search" placeholder="Enter a city Name" className="search-input" required/>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
