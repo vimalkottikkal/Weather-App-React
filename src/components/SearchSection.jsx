98d96bbfcdeb215400b03283cd1a27082ed54374
vimalkottikkal <vimalvijay1994@gmail.com>
2025-01-16 00:08:58 +0400

    weather-app v1

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -4,7 +4,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;
   const handleCitySearch = (e)=> {
     e.preventDefault();
     const searchInput = e.target.querySelector(".search-input")
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
     getWeatherDetails(API_URL);
     
   }
